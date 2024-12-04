f35e96d51f80d3098d5f391ad154e9519886a436
luketheobscure <luketheobscure@gmail.com>
2024-12-03 16:56:49 -0800

    Day 3

---
+++ day3/day3.ts
@@ -0,0 +1,32 @@
+const input = Deno.readTextFileSync("inputs/3.txt");
+
+function followInstructions(matcher: RegExp) {
+  const matches = input.matchAll(matcher);
+  let result = 0;
+  let skip = false;
+
+  for (const [match] of matches) {
+    if (match === "do()") {
+      skip = false;
+      continue;
+    }
+    if (match === "don't()") {
+      skip = true;
+      continue;
+    }
+    if (skip) continue;
+
+    const [a, b] = match.replace("mul(", "").replace(")", "").split(",").map(
+      Number,
+    );
+
+    result += a * b;
+  }
+
+  return result;
+}
+
+console.log(`Part 1: ${followInstructions(/mul\(\d+,\d+\)/g)}`);
+console.log(
+  `Part 2: ${followInstructions(/don\'t\(\)|do\(\)|mul\(\d+,\d+\)/g)}`,
+);
